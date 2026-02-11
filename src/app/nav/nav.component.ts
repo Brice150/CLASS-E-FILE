eb5cd6e51a73a80732fc69c150f94e1dd8c9e36a
brice150 <brice.lecomte0@gmail.com>
2026-02-11 12:09:40 +0100

    nav

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,7 +1,11 @@
 import { CommonModule } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, EventEmitter, inject, OnInit, Output } from '@angular/core';
 import { NavigationEnd, Router, RouterModule } from '@angular/router';
-import { filter } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { filter, Subject, takeUntil } from 'rxjs';
+import { Notification } from '../core/interfaces/notification';
+import { NotificationService } from '../core/services/notification.service';
 
 @Component({
   selector: 'app-nav',
@@ -13,6 +17,10 @@ export class NavComponent implements OnInit {
   @Output() logoutEvent = new EventEmitter<void>();
   menuOpen = false;
   router = inject(Router);
+  notificationService = inject(NotificationService);
+  toastr = inject(ToastrService);
+  notificationNumber: number = 0;
+  destroyed$ = new Subject<void>();
 
   ngOnInit(): void {
     this.router.events
@@ -22,6 +30,30 @@ export class NavComponent implements OnInit {
           this.menuOpen = false;
         }
       });
+
+    this.notificationService
+      .getNotifications()
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe({
+        next: (notifications: Notification[]) => {
+          this.notificationNumber = notifications.filter(
+            (notification) => !notification.read,
+          ).length;
+        },
+        error: (error: HttpErrorResponse) => {
+          if (!error.message.includes('Missing or insufficient permissions.')) {
+            this.toastr.error(error.message, 'Erreur', {
+              positionClass: 'toast-bottom-center',
+              toastClass: 'ngx-toastr custom error',
+            });
+          }
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
   }
 
   toggleMenu() {

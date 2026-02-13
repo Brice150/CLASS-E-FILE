adc7e4b31bfe5c58edf8ccaddfbfd80acc0bb351
brice150 <brice.lecomte0@gmail.com>
2026-02-13 18:45:34 +0100

    menu start

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -10,11 +10,12 @@ import { MatDialog } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { filter, Subject, switchMap, takeUntil } from 'rxjs';
 import { Article } from '../core/interfaces/article';
@@ -40,6 +41,8 @@ import { FilterDialogComponent } from '../shared/components/filter-dialog/filter
     MatChipsModule,
     EmptyCardComponent,
     MatSelectModule,
+    MatMenuModule,
+    RouterLink,
   ],
   templateUrl: './category.component.html',
   styleUrl: './category.component.css',
@@ -61,7 +64,7 @@ export class CategoryComponent implements OnInit, OnDestroy {
   isTouched = false;
   articleFilter: Article = {} as Article;
   overlay = inject(Overlay);
-  displayedColumns: string[] = ['title', 'genres', 'grade'];
+  displayedColumns: string[] = ['title', 'genres', 'grade', 'actions'];
   dataSource = new MatTableDataSource(this.filteredArticles);
   _liveAnnouncer = inject(LiveAnnouncer);
   genres: string[] = [];
